f79286a9748c29af36996d789a8041ecd70e3582
Olexandr Gorban <ukrgorban@gmail.com>
2022-10-06 16:02:28 +0300

    task: 1, 2

---
+++ JavaScript/a-task-1/config.js
@@ -0,0 +1,14 @@
+const config = {
+  db: {
+    host: "127.0.0.1",
+    port: 5432,
+    database: "test",
+    user: "test",
+    password: "test",
+  },
+  staticPort: 8000,
+  port: 8001,
+  transport: "http",
+};
+
+module.exports = config;
