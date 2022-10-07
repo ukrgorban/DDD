c0fc1e917a0bb2e9ef7ffbe44f4f01bc15cee57d
Olexandr Gorban <ukrgorban@gmail.com>
2022-10-07 15:48:47 +0300

    task: 3, 4

--- JavaScript/a-task-1/static/client.js
+++ JavaScript/a-task-1/static/client.js
@@ -1,44 +1,95 @@
-'use strict';
+"use strict";
+let socket;
 
-const socket = new WebSocket('ws://127.0.0.1:8001/');
+const getArgs = (args) => {
+  let id = "";
+  let data = {};
 
-const scaffold = (structure) => {
+  if (args.length) {
+    if (typeof args[0] === "object") {
+      data = args[0];
+    } else if (typeof +args[0] === "number") {
+      id = args[0];
+      data = args[1];
+    }
+  }
+
+  return { id, data };
+};
+
+const scaffold = (url, structure) => {
   const api = {};
   const services = Object.keys(structure);
+  const transport = url.split(":")[0];
+
+  if (transport === "ws") {
+    socket = new WebSocket(url);
+  }
+
   for (const serviceName of services) {
     api[serviceName] = {};
     const service = structure[serviceName];
     const methods = Object.keys(service);
     for (const methodName of methods) {
-      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-        const packet = { name: serviceName, method: methodName, args };
-        socket.send(JSON.stringify(packet));
-        socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          resolve(data);
-        };
-      });
+      api[serviceName][methodName] = (...args) => {
+        return new Promise(async (resolve) => {
+          if (transport === "http") {
+            const { id, data } = getArgs(args);
+            const res = await fetch(
+              `${url}${serviceName}/${methodName}/${id}`,
+              {
+                method: "POST",
+                body: JSON.stringify(data),
+              }
+            );
+            const result = await res.json();
+            resolve(result);
+          }
+
+          if (transport === "ws") {
+            const packet = { name: serviceName, method: methodName, args };
+            socket.send(JSON.stringify(packet));
+            socket.onmessage = (event) => {
+              const data = JSON.parse(event.data);
+              resolve(data);
+            };
+          }
+        });
+      };
     }
   }
   return api;
 };
 
-const api = scaffold({
+const api = scaffold("http://127.0.0.1:8001/", {
   user: {
-    create: ['record'],
-    read: ['id'],
-    update: ['id', 'record'],
-    delete: ['id'],
-    find: ['mask'],
+    create: ["record"],
+    read: ["id"],
+    update: ["id", "record"],
+    delete: ["id"],
+    find: ["mask"],
   },
   country: {
-    read: ['id'],
-    delete: ['id'],
-    find: ['mask'],
+    read: ["id"],
+    delete: ["id"],
+    find: ["mask"],
+    update: ["id", "record"],
+  },
+  city: {
+    read: ["id"],
+    create: ["record"],
+    delete: ["id"],
+    find: ["mask"],
+    update: ["id", "record"],
   },
 });
 
-socket.addEventListener('open', async () => {
-  const data = await api.user.read(3);
+// socket.addEventListener("open", async () => {
+//   const data = await api.user.read(3);
+//   console.dir({ data });
+// });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await api.city.read();
   console.dir({ data });
 });
